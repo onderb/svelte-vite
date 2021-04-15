6337a4ea800e799187bc2d1b7f036ea48a7e91b6
Onder Bakirtas <onderbakirtas@gmail.com>
2021-04-15 14:03:51 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Route } from "svelte-micro";
+  import About from "./About.svelte";
+  import Home from "./Home.svelte";
+</script>
+
+<Route>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+  </nav>
+  <Route path="/">
+    <Home />
+  </Route>
+  <Route path="/about">
+    <About />
+  </Route>
+  <Route fallback>
+    <h1>Route not found :(</h1>
+    <a href="/">Back to home</a>
+  </Route>
+</Route>
